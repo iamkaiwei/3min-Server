89ece1675459acb54ab31d33976dffbc9826de1f
anhtrantuan <anhtrantuan.hcmc@gmail.com>
2013-09-21 16:54:30 +0700

    Upgrade to Ruby 2 and Rails 4.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreeminsServer::Application.config.secret_key_base = '40e79af1f895e79c0d14828a5231e1d4c0c0be07b5ff2fcdbedce2d93bb4ab24c6098cdd8f0071b5bd7d9ec1d0889a9a8bf5c04f1733edef3d8b35d6cf7868d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThreeminsServer::Application.config.session_store :cookie_store, key: '_threemins-server_session'
