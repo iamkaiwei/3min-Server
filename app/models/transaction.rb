93e5efe4eb66a9faafe48eba085cfa2423d7520b
Anh Tran <anhtrantuan.hcmc@gmail.com>
2013-09-26 01:27:05 +0700

    Fix bug in Transaction model.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ActiveRecord::Base
 	belongs_to :product
-	belongs_to :buyer, :class => User
-	belongs_to :seller, :class => User
+	belongs_to :buyer, :class_name => "User"
+	belongs_to :seller, :class_name => "User"
 end
