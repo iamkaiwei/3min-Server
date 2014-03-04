8e7e9c547e5917663d91b2628b2aba1026bd6180
Tung Phan <nlds.a279@gmail.com>
2014-03-04 17:00:48 +0800

    UA production

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,5 +1,5 @@
-Urbanairship.application_key = 'yq6GxkLBSUCxjQ2cGJw5DA'
-Urbanairship.application_secret = 'HGyuL8agSXW7rBB9JJQILw'
-Urbanairship.master_secret = 'D8XjAksgSV2xujPpr33_Gg'
+Urbanairship.application_key = ENV['UA_APPLICATION_KEY']
+Urbanairship.application_secret = ENV['UA_APPLICATION_SECRET']
+Urbanairship.master_secret = ENV['UA_MASTER_SECRET']
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 5
