07c2318651239d3ab7c35ae4caf4b93c8665674e
Tung Phan <nlds.a279@gmail.com>
2014-03-20 10:58:00 +0700

    forgot to add routes

--- app/controllers/api/v1/pushers_controller.rb
+++ app/controllers/api/v1/pushers_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ConversationsController < Api::BaseController
+class Api::V1::PushersController < Api::BaseController
   def auth
     response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
     render :json => response
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,57 @@
 ThreeminsServer::Application.routes.draw do
   ActiveAdmin.routes(self)
-	use_doorkeeper do
-		controllers :tokens => "doorkeeper/custom_tokens"
-	end
+  use_doorkeeper do
+    controllers :tokens => "doorkeeper/custom_tokens"
+  end
 
-	devise_for :users
+  devise_for :users
 
-	root :to => "home#index"
+  root :to => "home#index"
 
-	namespace :api do
-		namespace :v1 do
-			resources :users, :only => [:index, :show, :update] do
-				collection do
-					get :current
-					get :existence
-					get :facebook
-				end
-			end
+  namespace :api do
+    namespace :v1 do
+      post 'pushers/auth', to: 'pushers#auth'
+      resources :users, :only => [:index, :show, :update] do
+        collection do
+          get :current
+          get :existence
+          get :facebook
+        end
+      end
 
-			resources :categories, :only => [:index, :show] do
-				collection do
-					get :taggable
-					get :display
-				end
-			end
+      resources :categories, :only => [:index, :show] do
+        collection do
+          get :taggable
+          get :display
+        end
+      end
 
-			resources :products, :except => [:new, :edit] do
-				resource :likes, only: [:create, :destroy]
+      resources :products, :except => [:new, :edit] do
+        resource :likes, only: [:create, :destroy]
 
-				collection do
-					get :me
-					get :offer
-					get :liked
-					get :search
-					get :popular
-				end
+        collection do
+          get :me
+          get :offer
+          get :liked
+          get :search
+          get :popular
+        end
 
-				get :show_offer, on: :member
-			end
+        get :show_offer, on: :member
+      end
 
-			resources :conversations, only: [:create, :index, :show] do
-				resources :conversation_replies, only: :create
-				put :offer, on: :member
-			end
+      resources :conversations, only: [:create, :index, :show] do
+        resources :conversation_replies, only: :create
+        put :offer, on: :member
+      end
 
-			resources :transactions, :except => [:new, :edit, :destroy] do
-				collection do
-					get :buys
-					get :sells
-				end
-			end
+      resources :transactions, :except => [:new, :edit, :destroy] do
+        collection do
+          get :buys
+          get :sells
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
