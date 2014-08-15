51494da23c82693a67746d3b522c8460acbe4f30
Tung Phan <nlds.a279@gmail.com>
2014-08-15 14:34:51 +0700

    Fix bug

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ActivitiesController < Api::BaseController
   def index
-    @activities = User.find(4).activities.order(updated_at: :desc).paginate(page: params[:page])
+    @activities = current_api_user.activities.order(updated_at: :desc).paginate(page: params[:page])
   end
 end
